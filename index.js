c2ccb1e6629a1b84264a98d43d744d840c246266
nikhilmodak <nikhil.modak@lithium.com>
2014-09-29 10:33:48 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,257 @@
+'use strict';
+
+var dom = require('./src/dom');
+var ngdoc = require('./src/ngdoc');
+var reader = require('./src/reader');
+
+var fs = require('fs');
+var vfs = require('vinyl-fs');
+var through2 = require('through2');
+var extend = require('extend');
+var _ = require('lodash');
+var gutil = require('gulp-util');
+var File = gutil.File;
+var PluginError = gutil.PluginError;
+var path = require('path');
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf8');
+var merge = require('merge-stream');
+
+var setup = {sections: {}, pages: [], apis: {}};
+var fakeDest = '_FAKE_DEST_';
+var templates = path.resolve(__dirname, 'src/templates');
+var bowerComponents = path.resolve(__dirname, 'bower_components');
+
+function copyTemplates() {
+  return function () {
+    return vfs.src(['**/*', '!**/*.tmpl'], {cwd: templates});
+  };
+}
+
+function streamFile(src, dir, dest, name) {
+  return function () {
+    return vfs.src(src).pipe(through2.obj(function (file, enc, callback) {
+      name = name === undefined ? file.path.split('/').pop() : name;
+      this.push(new File({
+        base: dest,
+        cwd: dest,
+        path: path.join(dest, dir, name),
+        contents: file.contents
+      }));
+      callback(null);
+    }));
+  };
+}
+
+function sections(sects) {
+  return merge(_.map(sects, function (data, key) {
+    if (data instanceof String) {
+      data = {glob: data};
+    }
+    if (!data.hasOwnProperty('glob')) {
+      throw new PluginError('gulp-ngdocs', 'Invalid sections, please refer to the documentation.');
+    }
+    if (!data.hasOwnProperty('title')) {
+      data.title = key;
+    }
+    if (!data.hasOwnProperty('api')) {
+      data.api = true;
+    }
+    var glob = data.glob;
+    var opts = data.globOpts;
+    setup.sections[key] = data.title;
+    setup.apis[key] = data.api;
+    return vfs.src(glob, opts)
+      .pipe(through2.obj(
+        function (file, enc, cb) {
+          file.section = key;
+          this.push(file);
+          cb(null);
+        }
+      ));
+    }));
+}
+
+function process(opts) {
+
+  var options = extend({
+    startPage: '/api',
+    scripts: [],
+    styles: [],
+    title: 'API Documentation',
+    html5Mode: true,
+    editExample: true,
+    navTemplate: false,
+    navContent: '',
+    navTemplateData: {}
+  }, opts);
+  var defaultSection = 'api';
+  var defaultScripts = [
+    path.join(bowerComponents, 'angular/angular.min.js'),
+    path.join(bowerComponents, 'angular/angular.min.js.map'),
+    path.join(bowerComponents, 'angular-animate/angular-animate.min.js'),
+    path.join(bowerComponents, 'marked/lib/marked.js'),
+    path.join(bowerComponents, 'google-code-prettify/src/prettify.js')
+  ];
+
+  function writeSetup() {
+    var options = setup.__options,
+        content, data = {
+          scripts: options.scripts,
+          styles: options.styles,
+          sections: _.keys(setup.sections).join('|'),
+          discussions: options.discussions,
+          analytics: options.analytics,
+          navContent: options.navContent,
+          title: options.title,
+          image: options.image,
+          titleLink: options.titleLink,
+          imageLink: options.imageLink,
+          bestMatch: options.bestMatch,
+          deferLoad: !!options.deferLoad
+        };
+
+    // create index.html
+    content = fs.readFileSync(path.resolve(templates, 'index.tmpl'), 'utf8');
+    content = _.template(content, data);
+    docsStream.push(new File({
+      base: fakeDest,
+      cwd: fakeDest,
+      path: path.join(fakeDest, 'index.html'),
+      contents: new Buffer(content, 'utf8')
+    }));
+    // create setup file
+    setup.html5Mode = options.html5Mode;
+    setup.editExample = options.editExample;
+    setup.startPage = options.startPage;
+    setup.discussions = options.discussions;
+    setup.scripts = _.map(options.scripts, function(url) { return path.basename(url); });
+    docsStream.push(new File({
+      base: fakeDest,
+      cwd: fakeDest,
+      path: setup.__file,
+      contents: new Buffer('NG_DOCS=' + JSON.stringify(setup, null, 2) + ';', 'utf8')
+    }));
+  }
+
+  function transformFunction (file, enc, callback) {
+    if (file.isNull()) {
+      callback(null);
+      return; // ignore
+    }
+    if (file.isStream()) {
+      callback(new gutil.PluginError('gulp-ngdocs', 'Streaming not supported'));
+      return;
+    }
+    if (file.contents) {
+      if (!merged) {
+        merged = merge(fstreams.map(function (f) {
+          var s = f();
+          s.on('data', function (file) {
+            docsStream.push(file);
+          });
+          return s;
+        }));
+
+        merged.on('end', function () {
+          mergedEnded = true;
+          if (docsStreamEndCb) {
+            docsStreamEndCb(null);
+          }
+        });
+      }
+      var content = decoder.write(file.contents);
+      if (!file.section) {
+        file.section = defaultSection;
+      }
+      reader.process(content, file.path, file.section, options);
+    }
+    callback(null);
+  }
+
+  function flushFunction (cb) {
+    if (merged) {
+      docsStreamEndCb = cb;
+      ngdoc.merge(reader.docs);
+      reader.docs.forEach(function(doc){
+        // this hack is here because on OSX angular.module and angular.Module map to the same file.
+        var id = doc.id.replace('angular.Module', 'angular.IModule').replace(':', '.'),
+            file = path.join(fakeDest, 'partials', doc.section, id + '.html'),
+            dir = path.join(fakeDest, 'partials', doc.section);
+        docsStream.push(new File({
+          base: fakeDest,
+          cwd: fakeDest,
+          path: file,
+          contents: new Buffer(doc.html(), 'utf8')
+        }));
+      });
+
+      ngdoc.checkBrokenLinks(reader.docs, setup.apis, options);
+
+      setup.pages = _.union(setup.pages, ngdoc.metadata(reader.docs));
+
+      writeSetup(this);
+
+      if (mergedEnded) {
+        docsStreamEndCb(null);
+        docsStreamEndCb = false;
+      }
+    } else {
+      cb(null);
+    }
+  }
+
+  var fstreams = [];
+  var docsStream = through2.obj(transformFunction, flushFunction);
+  var merged = false;
+  var mergedEnded = false;
+  var docsStreamEndCb = false;
+  if (options.navTemplate) {
+    options.navContent = _.template(
+      fs.readFileSync(options.navTemplate, 'utf8'), 
+      options.navTemplateData);
+  }
+
+  if (options.image) {
+    if (!/^((https?:)?\/\/|\.\.\/)/.test(options.image)) {
+      fstreams.push(streamFile(options.image, 'img', fakeDest));
+      options.image = 'img/' + options.image;
+    }
+  }
+
+  options.scripts = options.scripts.map(function (script) {
+    fstreams.push(streamFile(script, 'js', fakeDest));
+    return path.join('js', script.split('/').pop());
+  });
+
+  defaultScripts.forEach(function (script, i) {
+    fstreams.push(streamFile(script, 'js', fakeDest));
+    options.scripts.splice(i, 0, path.join('js', script.split('/').pop()));
+  });
+
+  options.styles = _.map(options.styles, function(file) {
+    if (/^((https?:)?\/\/|\.\.\/)/.test(file)) {
+      return file;
+    } else {
+      fstreams.push(streamFile(file, 'img', fakeDest));
+      return 'css/' + file.split('/').pop();
+    }
+  });
+
+  fstreams.push(copyTemplates(templates));
+  setup.__file = path.join(fakeDest, 'js/docs-setup.js');
+  setup.__options = options;
+
+  reader.docs = [];
+  if (Object.keys(setup.sections).length === 0) {
+    setup.sections[defaultSection] = options.title;
+    setup.apis[defaultSection] = true;
+  }
+
+  return docsStream;
+}
+
+module.exports = {
+  process: process,
+  sections: sections
+};
