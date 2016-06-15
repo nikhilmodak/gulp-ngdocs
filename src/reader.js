be54c9e9c327870205424d0544ba65c24d3616d5
Tschoepe <christian.tschoepe@solarwinds.com>
2016-06-15 14:02:43 -0500

    Fix for .ts files

--- src/reader.js
+++ src/reader.js
@@ -13,11 +13,7 @@ function process(content, file, section, options) {
   // console.log('-section-----\n' + section)
   // console.log('-options-----\n' + JSON.stringify(options, null, 2))
   // console.log('')
-  if (/\.js$/.test(file)) {
-    processJsFile(content, file, section, options).forEach(function(doc) {
-      exports.docs.push(doc);
-    });
-  } else if (/\.ts$/.test(file)) {
+  if (/(\.js|\.ts)$/.test(file)) {
     processJsFile(content, file, section, options).forEach(function(doc) {
       exports.docs.push(doc);
     });
